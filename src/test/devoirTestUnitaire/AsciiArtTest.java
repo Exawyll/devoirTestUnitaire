690f93acce6dc69b5f4d975d6109efecdccd1003
Jérôme Pavic <jeromepavic@gmail.com>
2017-06-19 16:59:44 +0200

    Work on tests

--- src/test/devoirTestUnitaire/AsciiArtTest.java
+++ src/test/devoirTestUnitaire/AsciiArtTest.java
@@ -5,6 +5,11 @@ package devoirTestUnitaire;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import model.AsciiArt;
+
 import org.junit.Test;
 
 /**
@@ -18,7 +23,27 @@ public class AsciiArtTest {
 	 */
 	@Test
 	public void testAsciiArt() {
-		fail("Not yet implemented");
-	}
 
+		try {
+			AsciiArt aArt = new AsciiArt(1);
+			HashMap<String, ArrayList<String>> alphabet = aArt.getAlphabet();
+			assertEquals(27, alphabet.size());
+		} catch (Exception e) {
+			fail("exception thrown");
+		}
+		
+		try {
+			AsciiArt aArt = new AsciiArt(2);
+			HashMap<String, ArrayList<String>> alphabet = aArt.getAlphabet();
+			assertEquals(27, alphabet.size());
+		} catch (Exception e) {
+			fail("exception thrown");
+		}
+		
+	}
+	
+	@Test (expected = Exception.class)
+	public void testAsciiArtExcep() throws Exception {
+		AsciiArt aArt = new AsciiArt(3);
+	}
 }
