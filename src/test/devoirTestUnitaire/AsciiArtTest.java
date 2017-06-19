5ff7a0b1fbdd77bb469983adb919c4eb2c7a464e
Jérôme Pavic <jeromepavic@gmail.com>
2017-06-19 16:30:51 +0200

    create AsciiArtTest

---
+++ src/test/devoirTestUnitaire/AsciiArtTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package devoirTestUnitaire;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author jerome
+ *
+ */
+public class AsciiArtTest {
+
+	/**
+	 * Test method for {@link model.AsciiArt#AsciiArt(int)}.
+	 */
+	@Test
+	public void testAsciiArt() {
+		fail("Not yet implemented");
+	}
+
+}
