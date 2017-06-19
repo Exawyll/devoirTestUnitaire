794c4dccfb52ac07adfce729b7d04406078087dd
Wylliam <wylliam.delaunay@gmail.com>
2017-06-19 14:52:25 +0200

    model

--- src/core/main/Application.java
+++ src/core/main/Application.java
@@ -1,9 +1,33 @@
 package main;
 
+import java.util.Scanner;
+
 public class Application {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("prout");
+		Scanner in = new Scanner(System.in);
+        // Width = 20 and 9
+		int L = in.nextInt();
+        
+        // Height = 11 and 9
+        int H = in.nextInt();
+        
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        
+        String T = in.nextLine();
+        
+        for (int i = 0; i < H; i++) {
+            String ROW = in.nextLine();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
 
 	}
 
